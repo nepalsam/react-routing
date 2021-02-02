6d45b78838870626d3bc06e2f19e38faded21274
unknown <you@example.com>
2021-02-02 15:36:57 -0500

    modify files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route,Switch } from 'react-router-dom';
+//import { Route, Switch} from 'react-router';
 
+
+import './App.css';
+import Welcome from './component/welcome/Welcome';
+import Clock from './component/clock/Clock';
+import Contact from './component/contact/Contact';
+import Navigation from './component/navigation/Navigation';
+import NotFound from './component/notfound/NotFound';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <Switch>
+      <Route 
+      exact path="/" 
+      render={(props)=> <Welcome{...props} name="Sam"/>} 
+      />
+     <Route path="/clock" component= {Clock} />
+     <Route path="/contact" component= {Contact} />
+     <Route path="/welcome/:name" component= {Welcome} />
+
+     <Route>
+       <NotFound/>
+     </Route>
+      </Switch> 
+     
     </div>
   );
 }
 
 export default App;
+
+
+
